50b7fde3128250f2ff2d8c5ba68ebd82e7643ee7
Szymon <contact@szymonlorenz.com>
2025-02-22 18:57:39 +1000

    Add trailing stop helper

--- IBKit/IBKit/Model/IBOrder+Ext.swift
+++ IBKit/IBKit/Model/IBOrder+Ext.swift
@@ -26,6 +26,19 @@ public extension IBOrder {
 	static func stopLimit(stop: Double, limit: Double, action: IBAction, quantity: Double,  contract: IBContract, account: String, validUntil tif: TimeInForce = .day, hidden: Bool = true, extendedTrading: Bool = false) -> IBOrder {
 		return IBOrder(contract: contract, action: action, totalQuantity: quantity, orderType: .STOP_LIMIT, lmtPrice: limit, auxPrice: stop, tif: tif, outsideRth: extendedTrading, hidden: hidden, account: account)
 	}
-		
+    
+    /// Creates a **Trailing Stop Order**
+    /// - Parameters:
+    ///   - stopOffset: The offset amount from the market price where the stop-loss should trail.
+    ///   - action: `.buy` or `.sell`
+    ///   - quantity: Number of shares/contracts
+    ///   - contract: The asset contract
+    ///   - account: IB account ID
+    ///   - tif: Time In Force (default `.day`)
+    ///   - hidden: Whether the order should be hidden
+    ///   - extendedTrading: Whether it should execute outside regular trading hours
+    static func trailingStop(stopOffset: Double, action: IBAction, quantity: Double, contract: IBContract, account: String, validUntil tif: TimeInForce = .day, hidden: Bool = true, extendedTrading: Bool = false) -> IBOrder {
+        IBOrder(contract: contract, action: action, totalQuantity: quantity, orderType: .TRAILING, lmtPrice: nil, auxPrice: stopOffset, tif: tif, outsideRth: extendedTrading, hidden: hidden, account: account)
+    }
 }
 
